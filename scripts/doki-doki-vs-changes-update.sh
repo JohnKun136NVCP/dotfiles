829a80873024e11ab4644ce5c433a27701841da9
JohnKun136NVCP <johnb@ciencias.unam.mx>
2023-10-21 15:17:48 -0600

    [FIXED][MAIN]
    Fixed installation for Fix VSCode Checksums and Doki Theme extension for VS

--- scripts/doki-doki-vs-changes-update.sh
+++ scripts/doki-doki-vs-changes-update.sh
@@ -1,1 +1,7 @@
+#To fix errors before apply doki-doki themes
+code --install-extension -lehni.vscode-fix-checksums
+#Permissions to extension
+sudo chown -R $(whoami) /usr/share/code/
+#if you've installed doki-doki themes before
 sudo chown -R $(whoami) /usr/share/code/resources/app/out/vs/workbench
+
