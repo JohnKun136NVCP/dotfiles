10fd02405ac3233ae331f2a57141753a870c90c1
桂馬達也 <73410627+JohnKun136NVCP@users.noreply.github.com>
2025-07-28 05:36:31 +0900

    Update converter.sh

--- FastFetchConfig/converter.sh
+++ FastFetchConfig/converter.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#!/bin/bash
-for file in *.jpg; do
+for file in *.jpg *.jpeg; do
     # Check if the file exists (to avoid errors if no .jpg files are found)
     if [ -f "$file" ]; then
         # Rename the file to .png
