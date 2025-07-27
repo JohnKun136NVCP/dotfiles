dd2c897a371156dba31343f7ad56d206fb3f5055
桂馬達也 <73410627+JohnKun136NVCP@users.noreply.github.com>
2025-07-28 05:24:05 +0900

    Update wallpaperKDE.sh

--- scriptsKDE/ChangeWallpaper/wallpaperKDE.sh
+++ scriptsKDE/ChangeWallpaper/wallpaperKDE.sh
@@ -4,28 +4,17 @@ export DISPLAY=:0
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
 wallpaperDir="$1"
+wallpaperRandomFile1="$wallpaperDir/$(ls "$wallpaperDir" | shuf -n 1)"
+wallpaperRandomFile2="$wallpaperDir/$(ls "$wallpaperDir" | shuf -n 1)"
 
-if [ ! -d "$wallpaperDir" ]; then
-    echo "Error: Wallpaper directory not found!"
-    exit 1
-fi
-
-wallpaper1="file://$wallpaperDir/$(ls "$wallpaperDir" | shuf -n 1)"
-wallpaper2="file://$wallpaperDir/$(ls "$wallpaperDir" | shuf -n 1)"
-
-dbus-send --session --dest=org.kde.plasmashell \
---type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript string:"
-var allDesktops = desktops();
-for (var i = 0; i < allDesktops.length; i++) {
-    d = allDesktops[i];
-    if (d.screen === 0) {
-        d.wallpaperPlugin = 'org.kde.image';
-        d.currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General'];
-        d.writeConfig('Image', '$wallpaper1');
-    } else if (d.screen === 1) {
-        d.wallpaperPlugin = 'org.kde.image';
-        d.currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General'];
-        d.writeConfig('Image', '$wallpaper2');
-    }
-}
+qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "
+var Desktops = desktops();
+Desktops[0].wallpaperPlugin = 'org.kde.image';
+Desktops[0].currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General'];
+Desktops[0].writeConfig('Image', 'file://${wallpaperRandomFile1}');
+Desktops[0].reloadConfig();
+Desktops[1].wallpaperPlugin = 'org.kde.image';
+Desktops[1].currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General'];
+Desktops[1].writeConfig('Image', 'file://${wallpaperRandomFile2}');
+Desktops[1].reloadConfig();
 "
